c73c8d386817fe91613c902725e6663aa6b984d2
nikusha09 <nikusha.bakhtadze123@gmail.com>
2026-03-18 19:49:31 +0400

    modified model classes as entities

--- Spring-Core/src/main/java/com/gym/model/Training.java
+++ Spring-Core/src/main/java/com/gym/model/Training.java
@@ -1,27 +1,43 @@
 package com.gym.model;
 
-import lombok.AllArgsConstructor;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.time.Duration;
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString
+@Entity
+@Table(name = "trainings")
 public class Training {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long traineeId;
-    private Long trainerId;
-    private String trainingName;
-    private String trainingType;
-    private LocalDate trainingDate;
-    private Duration trainingDuration;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "trainee_id", nullable = false)
+    private Trainee trainee;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "trainer_id", nullable = false)
+    private Trainer trainer;
+
+    @Column(name = "training_name", nullable = false)
+    private String trainingName;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "training_type_id", nullable = false)
+    private TrainingType trainingType;
+
+    @Column(name = "training_date", nullable = false)
+    private LocalDate trainingDate;
+
+    @Column(name = "training_duration", nullable = false)
+    private int trainingDuration;
 }
--- Spring-Core/src/main/java/com/gym/model/User.java
+++ Spring-Core/src/main/java/com/gym/model/User.java
@@ -1,6 +1,6 @@
 package com.gym.model;
 
-
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,12 +12,26 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString(exclude = "password")
+@Entity
+@Table(name = "users")
 public class User {
 
-    private String firstName;
-    private String lastName;
-    private String username;
-    private String password;
-    private boolean isActive;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "is_active", nullable = false)
+    private boolean isActive;
 }
